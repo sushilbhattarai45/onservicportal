4503965e660e0436df41772a779efd62f6a421b2
Aashish Panthi <aashishpanthi11@gmail.com>
2022-11-11 23:36:52 +0545

    complete user table

--- pages/users.js
+++ pages/users.js
@@ -1,50 +1,80 @@
-import {
-  Grid,
-  Link,
-  Button,
-  Table,
-  TableBody,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TableCell,
-  TablePagination,
-} from "@mui/material";
-import EditIcon from "@material-ui/icons/Edit";
-import BaseCard from "../src/components/baseCard/BaseCard";
+import { Grid, IconButton } from "@mui/material";
 
-import { useState } from "react";
+import axios from "axios";
+
+import BaseCard from "../src/components/baseCard/BaseCard";
+import MaterialTable from "material-table";
+
+import { useEffect, useState } from "react";
+
+import { forwardRef } from "react";
+
+import AddBox from "@material-ui/icons/AddBox";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import Check from "@material-ui/icons/Check";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import Clear from "@material-ui/icons/Clear";
+import DeleteOutline from "@material-ui/icons/DeleteOutline";
+import Edit from "@material-ui/icons/Edit";
+import FilterList from "@material-ui/icons/FilterList";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Remove from "@material-ui/icons/Remove";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
 
 const columns = [
-  { id: "user_name", label: "Username", minWidth: 90 },
-  { id: "user_email", label: "Email", minWidth: 120 },
-  { id: "user_district", label: "District", minWidth: 75 },
-  { id: "user_city", label: "City", minWidth: 60 },
-  { id: "user_street", label: "Street", minWidth: 75 },
-  { id: "user_contact", label: "Contact", minWidth: 75 },
-  { id: "user_gender", label: "Gender", minWidth: 75 },
   {
-    id: "user_profileImage",
-    label: "Profile Image",
-    minWidth: 95,
+    field: "user_profileImage",
+    title: "Avatar",
+    align: "center",
+    render: (rowData) => (
+      <img
+        src={rowData.user_profileImage}
+        style={{ width: 40, borderRadius: "50%" }}
+      />
+    ),
+  },
+  { field: "user_name", title: "Username" },
+  { field: "user_email", title: "Email" },
+  { field: "user_district", title: "District" },
+  { field: "user_city", title: "City" },
+  { field: "user_street", title: "Street" },
+  { field: "user_contact", title: "Contact" },
+  { field: "user_gender", title: "Gender" },
+  {
+    field: "user_status",
+    title: "User Status",
     align: "center",
   },
   {
-    id: "user_status",
-    label: "User Status",
-    minWidth: 90,
+    field: "edit",
+    title: "Edit",
     align: "center",
-  },
-  {
-    id: "action",
-    label: "Action",
-    minWidth: 85,
-    align: "center",
+    render: (rowData) => (
+      <div>
+        <IconButton
+          style={{
+            backgroundColor: "transparent",
+            border: "none",
+            outline: "none",
+          }}
+          onClick={() => {
+            console.log(rowData._id);
+          }}
+        >
+          <Edit />
+        </IconButton>
+      </div>
+    ),
   },
 ];
 
 const originalRows = [
   {
+    _id: "63575c4ee05ce8d0bee1dda8",
     user_name: "Ram nam",
     user_email: "a@g.c",
     user_district: "Achham",
@@ -56,10 +86,10 @@ const originalRows = [
     user_profileImage:
       "http://172.105.253.132:3001/uploads/6e150223-d75b-4c16-84d9-b416aebfa7ac9485ccad-1a49-4f8c-90ec-958a8b817589.jpeg",
     user_toc: { date: "Oct 25, 2022", time: "9:48 AM" },
-    __v: { $numberInt: "0" },
     user_status: "False",
   },
   {
+    _id: "63575e5ce05ce8d0bee1ddb5",
     user_name: "Saroj neupane",
     user_email: "A@b.c",
     user_district: "Achham",
@@ -71,9 +101,11 @@ const originalRows = [
     user_profileImage:
       "http://192.168.100.11:3001/uploads/2ab58f6a-72e3-49fa-863a-17897268dd4fc4f61c94-0701-4cbe-a7a9-95f35d71e212.jpeg",
     user_toc: { date: "Oct 25, 2022", time: "9:48 AM" },
-    __v: { $numberInt: "0" },
+    user_status: "False",
   },
 ];
+
+//GIVEN_API_KEY=AXCF
 
 const Users = () => {
   const [rows, setRows] = useState(originalRows);
@@ -81,88 +113,65 @@ const Users = () => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(7);
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
+  const getAllUsers = async () => {
+    try {
+      const { data } = axios.post(
+        "http://172.104.188.69:3001/v1/api/user/getAllUser",
+        {
+          GIVEN_API_KEY: "AXCF",
+        }
+      );
+
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
+  useEffect(() => {
+    // getAllUsers();
+  }, []);
+
+  const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => (
+      <ChevronRight {...props} ref={ref} />
+    )),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => (
+      <ChevronLeft {...props} ref={ref} />
+    )),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => (
+      <ArrowDownward {...props} ref={ref} />
+    )),
+    ThirdStateCheck: forwardRef((props, ref) => (
+      <Remove {...props} ref={ref} />
+    )),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
   };
 
   return (
     <Grid container spacing={0}>
       <Grid item xs={12} lg={12}>
         <BaseCard title="Users">
-          <TableContainer sx={{ maxHeight: 550 }}>
-            <Table stickyHeader aria-label="Users detail">
-              <TableHead>
-                <TableRow>
-                  {columns.map((column) => (
-                    <TableCell
-                      key={column.id}
-                      align={column.align}
-                      style={{ minWidth: column.minWidth }}
-                    >
-                      {column.label}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                  .map((row) => {
-                    return (
-                      <TableRow
-                        hover
-                        role="checkbox"
-                        tabIndex={-1}
-                        key={row.id}
-                      >
-                        {columns.map((column) => {
-                          if (column.id !== "action") {
-                            const value = row[column.id];
-                            return (
-                              <TableCell key={column.id} align={column.align}>
-                                {column.format && typeof value === "number"
-                                  ? column.format(value)
-                                  : value}
-                              </TableCell>
-                            );
-                          } else {
-                            return (
-                              <TableCell key={column.id} align="right">
-                                <Button
-                                  color="secondary"
-                                  component={Link}
-                                  to={`/edit-post/${row.id}`}
-                                  startIcon={<EditIcon />}
-                                  variant="contained"
-                                  size="small"
-                                  style={{ margin: "5px" }}
-                                >
-                                  Edit
-                                </Button>
-                              </TableCell>
-                            );
-                          }
-                        })}
-                      </TableRow>
-                    );
-                  })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[7, 15, 25]}
-            component="div"
-            count={rows ? rows.length : 0}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
+          <div style={{ maxWidth: "100%" }}>
+            <MaterialTable
+              icons={tableIcons}
+              columns={columns}
+              data={rows}
+              title="All of the users"
+            />
+          </div>
         </BaseCard>
       </Grid>
     </Grid>
