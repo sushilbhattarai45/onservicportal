1ac0ba96e23f0d6ff4bf5438253aed52a8fe4db6
Aashish Panthi <aashishpanthi11@gmail.com>
2022-11-11 23:47:41 +0545

    format code

--- pages/users.js
+++ pages/users.js
@@ -1,29 +1,27 @@
 import { Grid, IconButton } from "@mui/material";
-
 import axios from "axios";
-
 import BaseCard from "../src/components/baseCard/BaseCard";
 import MaterialTable from "material-table";
-
 import { useEffect, useState } from "react";
-
 import { forwardRef } from "react";
 
-import AddBox from "@material-ui/icons/AddBox";
-import ArrowDownward from "@material-ui/icons/ArrowDownward";
-import Check from "@material-ui/icons/Check";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
-import Clear from "@material-ui/icons/Clear";
-import DeleteOutline from "@material-ui/icons/DeleteOutline";
-import Edit from "@material-ui/icons/Edit";
-import FilterList from "@material-ui/icons/FilterList";
-import FirstPage from "@material-ui/icons/FirstPage";
-import LastPage from "@material-ui/icons/LastPage";
-import Remove from "@material-ui/icons/Remove";
-import SaveAlt from "@material-ui/icons/SaveAlt";
-import Search from "@material-ui/icons/Search";
-import ViewColumn from "@material-ui/icons/ViewColumn";
+import {
+  AddBox,
+  ArrowDownward,
+  Check,
+  ChevronLeft,
+  ChevronRight,
+  Clear,
+  DeleteOutline,
+  Edit,
+  FilterList,
+  FirstPage,
+  LastPage,
+  Remove,
+  SaveAlt,
+  Search,
+  ViewColumn,
+} from "@material-ui/icons";
 
 const columns = [
   {
@@ -110,9 +108,6 @@ const originalRows = [
 const Users = () => {
   const [rows, setRows] = useState(originalRows);
 
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(7);
-
   const getAllUsers = async () => {
     try {
       const { data } = axios.post(
@@ -123,6 +118,7 @@ const Users = () => {
       );
 
       console.log(data);
+      setRows(data);
     } catch (err) {
       console.log(err);
     }
