65336a0a32273b7e2c2a6f104037e22b6d7e8ddc
Aashish Panthi <aashishpanthi11@gmail.com>
2022-11-12 12:29:12 +0545

    controlled text inputs

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -9,16 +9,44 @@ import {
 } from "@mui/material";
 import BaseCard from "../../src/components/baseCard/BaseCard";
 
+import { useState } from "react";
+
 const EditProfile = () => {
+  const [values, setValues] = useState({
+    user_name: "",
+    user_email: "",
+    user_district: "",
+    user_city: "",
+    user_street: "",
+    user_contact: "",
+    user_gender: "",
+    user_password: "",
+    user_profileImage: "",
+  });
+
   const handleFilesChange = (files) => {
     // Do something...
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(values);
   };
 
   return (
     <Grid container spacing={0}>
       <Grid item xs={12} lg={12}>
         <BaseCard title="User edit">
-          <form>
+          <form onSubmit={handleFormSubmit}>
             <Stack spacing={2} direction="row" alignItems="center">
               <img src="https://via.placeholder.com/150" alt="user" />
               <FormControl>
@@ -28,6 +56,7 @@ const EditProfile = () => {
                   onChange={handleFilesChange}
                   label="Upload Image"
                   accept="image/*"
+                  name="user_profileImage"
                 />
               </FormControl>
             </Stack>
@@ -39,10 +68,26 @@ const EditProfile = () => {
             >
               <FormControl sx={{ my: 1, mr: 2 }} fullWidth>
                 <InputLabel htmlFor="name">Name</InputLabel>
-                <OutlinedInput id="name" name="name" type="text" label="name" />
+                <OutlinedInput
+                  id="name"
+                  name="user_name"
+                  type="text"
+                  label="name"
+                  onChange={handleInputChange}
+                  value={values.user_name}
+                  required
+                />
               </FormControl>
 
-              <TextField fullWidth name="email" type="text" label="Email" />
+              <TextField
+                fullWidth
+                name="user_email"
+                type="email"
+                label="Email"
+                onChange={handleInputChange}
+                value={values.user_email}
+                required
+              />
             </Stack>
             <Stack
               direction="row"
@@ -54,16 +99,32 @@ const EditProfile = () => {
                 <InputLabel htmlFor="district">District</InputLabel>
                 <OutlinedInput
                   id="district"
-                  name="district"
+                  name="user_district"
                   type="text"
                   label="district"
+                  onChange={handleInputChange}
+                  value={values.user_district}
                 />
               </FormControl>
               <FormControl sx={{ mx: 2 }} fullWidth>
                 <InputLabel htmlFor="city">City</InputLabel>
-                <OutlinedInput id="city" name="city" type="text" label="city" />
+                <OutlinedInput
+                  id="city"
+                  name="user_city"
+                  type="text"
+                  label="city"
+                  onChange={handleInputChange}
+                  value={values.user_city}
+                />
               </FormControl>
-              <TextField fullWidth name="street" type="text" label="Street" />
+              <TextField
+                fullWidth
+                name="user_street"
+                type="text"
+                label="Street"
+                onChange={handleInputChange}
+                value={values.user_street}
+              />
             </Stack>
             <Stack
               direction="row"
@@ -73,22 +134,28 @@ const EditProfile = () => {
             >
               <TextField
                 fullWidth
-                name="contact"
+                name="user_contact"
                 type="number"
                 label="Contact"
+                onChange={handleInputChange}
+                value={values.user_contact}
               />
               <TextField
                 fullWidth
-                name="gender"
+                name="user_gender"
                 type="text"
                 label="Gender"
                 sx={{ mx: 2 }}
+                onChange={handleInputChange}
+                value={values.user_gender}
               />
               <TextField
                 fullWidth
-                name="password"
+                name="user_password"
                 type="text"
                 label="Password"
+                onChange={handleInputChange}
+                value={values.user_password}
               />
             </Stack>
 
@@ -98,7 +165,7 @@ const EditProfile = () => {
               justifyContent="flex-end"
               my={2}
             >
-              <Button variant="contained" color="success">
+              <Button variant="contained" color="success" type="submit">
                 Update
               </Button>
             </Stack>
