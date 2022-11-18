f15e86f85377a4e2036432c020c71720c6347414
Aashish Panthi <aashishpanthi11@gmail.com>
2022-11-18 19:08:56 +0545

    cleared the files

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  staticPageGenerationTimeout: 1000,
-};
-
-module.exports = nextConfig;
