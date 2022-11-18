4b3e65d4a898069f19731ee5cb62fb41a6b7255c
Aashish Panthi <aashishpanthi11@gmail.com>
2022-11-18 23:23:24 +0545

    Add a service provider edit page (#4)
    * add user edit page
    
    Co-authored-by: Sushil Bhattarai <bhattaraisushil005@gmail.com>
    
    * add service-provider edit page
    
    Co-authored-by: Sushil Bhattarai <bhattaraisushil005@gmail.com>
    
    * add informative toast to service-provider edit page
    
    Co-authored-by: Sushil Bhattarai <bhattaraisushil005@gmail.com>
    
    Co-authored-by: Sushil Bhattarai <bhattaraisushil005@gmail.com>

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -3,5 +3,6 @@ export { default as DashboardApp } from "./DashboardApp";
 export { default as Categories } from "./Categories";
 export { default as AdminRegister } from "./Register";
 export { default as EditUser } from "./EditUser";
+export { default as EditSP } from "./EditSP";
 export { default as User } from "./User";
 export { default as ServiceProviders } from "./ServiceProviders";
