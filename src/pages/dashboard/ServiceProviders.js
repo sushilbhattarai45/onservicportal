61a828ab9cb68a593149f1309974858a745fcc21
Aashish Panthi <aashishpanthi11@gmail.com>
2022-11-27 09:45:53 +0545

    Open edit tab on name click

--- src/pages/dashboard/ServiceProviders.js
+++ src/pages/dashboard/ServiceProviders.js
@@ -279,16 +279,24 @@ export default function ServiceProviders() {
                           />
                         </TableCell>
                         <TableCell component="th" scope="row" padding="none">
-                          <Stack
-                            direction="row"
-                            alignItems="center"
-                            spacing={2}
+                          <Link
+                            to={`edit/${sp_contact}`}
+                            style={{
+                              textDecoration: "none",
+                              color: "inherit",
+                            }}
                           >
-                            <Avatar alt={sp_name} src={sp_profileImage} />
-                            <Typography variant="subtitle2" noWrap>
-                              {sp_name}
-                            </Typography>
-                          </Stack>
+                            <Stack
+                              direction="row"
+                              alignItems="center"
+                              spacing={2}
+                            >
+                              <Avatar alt={sp_name} src={sp_profileImage} />
+                              <Typography variant="subtitle2" noWrap>
+                                {sp_name}
+                              </Typography>
+                            </Stack>
+                          </Link>
                         </TableCell>
                         <TableCell align="left">{sp_bio}</TableCell>
                         <TableCell align="center">
--- src/pages/dashboard/SubCategory.js
+++ src/pages/dashboard/SubCategory.js
@@ -280,16 +280,24 @@ export default function SubCategories() {
                           />
                         </TableCell>
                         <TableCell component="th" scope="row" padding="none">
-                          <Stack
-                            direction="row"
-                            alignItems="center"
-                            spacing={2}
+                          <Link
+                            to={`edit/${_id}`}
+                            style={{
+                              textDecoration: "none",
+                              color: "inherit",
+                            }}
                           >
-                            <Avatar alt={subCat_name} src={subCat_photo} />
-                            <Typography variant="subtitle2" noWrap>
-                              {subCat_name}
-                            </Typography>
-                          </Stack>
+                            <Stack
+                              direction="row"
+                              alignItems="center"
+                              spacing={2}
+                            >
+                              <Avatar alt={subCat_name} src={subCat_photo} />
+                              <Typography variant="subtitle2" noWrap>
+                                {subCat_name}
+                              </Typography>
+                            </Stack>
+                          </Link>
                         </TableCell>
                         <TableCell align="left">{category}</TableCell>
                         <TableCell align="left">
